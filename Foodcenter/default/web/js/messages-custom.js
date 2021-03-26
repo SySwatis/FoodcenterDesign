0e11206e61322bb3537102316ffab90bfc1aa46e
syswatis <syswatis@gmail.com>
2021-03-26 15:44:40 +0100

    Hide message event click

---
+++ Foodcenter/default/web/js/messages-custom.js
@@ -0,0 +1,70 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * @api
+ */
+ define([
+    'jquery',
+    'uiComponent',
+    'Magento_Customer/js/customer-data',
+    'underscore',
+    'escaper',
+    'jquery/jquery-storageapi'
+], function ($, Component, customerData, _, escaper) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            cookieMessages: [],
+            messages: [],
+            allowedTags: ['div', 'span', 'b', 'strong', 'i', 'em', 'u', 'a'],
+            selector: '.page.messages .message'
+        },
+
+        /**
+         * Extends Component object by storage observable messages.
+         */
+        initialize: function () {
+            this._super();
+            
+            this.cookieMessages = _.unique($.cookieStorage.get('mage-messages'), 'text');
+            this.messages = customerData.get('messages').extend({
+                disposableCustomerData: 'messages'
+            });
+
+            // Force to clean obsolete messages
+            if (!_.isEmpty(this.messages().messages)) {
+                customerData.set('messages', {});
+            }
+
+            $.cookieStorage.set('mage-messages', '');
+        },
+
+        /**
+         * Prepare the given message to be rendered as HTML
+         *
+         * @param {String} message
+         * @return {String}
+         */
+        prepareMessageForHtml: function (message) {
+            return escaper.escapeHtml(message, this.allowedTags);
+        },
+
+        /**
+         * Extends Component object by storage observable messages.
+         */
+        hideMessage: function() {
+            var self = this;
+            $(self.selector).on('click',function(){
+                $(this).hide();
+            });
+            setTimeout(function () {
+                $(self.selector).hide();
+            }, 5000);
+        }
+
+    });
+});
